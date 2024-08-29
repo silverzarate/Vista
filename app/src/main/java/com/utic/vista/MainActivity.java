3562301d3fdfd4ca6544b30d7f4d8c8683193ca3
silver <silvestrezaratebalmori@gmail.com>
2024-08-29 11:06:45 -0400

    Initial commit

---
+++ app/src/main/java/com/utic/vista/MainActivity.java
@@ -0,0 +1,40 @@
+package com.utic.vista;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1;
+    private int num;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=findViewById(R.id.et1);
+        num=(int)(Math.random()*100001);
+        String cadena=String.valueOf(num);
+        Toast notificacion= Toast.makeText(this,cadena,Toast.LENGTH_LONG);
+        notificacion.show();
+    }
+    public void controlar(View v) {
+        String valorIngresado=et1.getText().toString();
+        int valor=Integer.parseInt(valorIngresado);
+        if (valor==num) {
+            Toast notificacion=Toast.makeText(this, "Muy bien recordaste el número mostrado",Toast.LENGTH_LONG);
+                    notificacion.show();
+        }
+        else {
+            Toast notificacion=Toast.makeText(this,"Lo siento pero no es el número que mostré.", Toast.LENGTH_LONG);
+                    notificacion.show();
+        }
+    }
+}
